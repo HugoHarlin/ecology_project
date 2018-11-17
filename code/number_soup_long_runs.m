23f49d8cba4c1a1c85b8f136609375d86c1c1021
HugoHarlin <36413511+HugoHarlin@users.noreply.github.com>
2018-11-17 16:53:53 +0100

    more display and more verification

--- code/number_soup_long_runs.m
+++ code/number_soup_long_runs.m
@@ -1,22 +1,18 @@
 % Number soup - A study of a simple ecosystem model
-%close all
+close all
 clear
-%clc
+clc
 seed = ceil(rand*10000);
-rng(seed);
-%rng(1)
+rng(1);
 tic
-numIter = 500000000; %number of iterations
+numIter = 750000; %number of iterations
 a = 5; % constant affecting reproduction rate
 dim = 2; %number of metabolites in system
 u = 1; % influx metabolite
 p = 0.001; % dictates the mutation probability
 my = 500; %constant dictating the increase of metabolite
 r = zeros(1,dim); % metabolites in system
-rData = zeros(dim,numIter+1); % metabolites in system
 
-tData = zeros(1,numIter+1);
-dtData =zeros(1,numIter+1);
 t = 0;
 dt = 0;
 
@@ -26,8 +22,13 @@ else
     species = ones(1, dim + (dim)*((dim-1)/2));
 end
 
-numSpecies = length(species);
-speciesData = zeros(numSpecies,numIter +1);
+numSpecies = length(species); % number of species
+dataFreq = 1; % storing frequency of data
+speciesData = zeros(numSpecies,ceil(numIter/dataFreq)); %species data
+rData = zeros(dim,ceil(numIter/dataFreq)); % metabolites in system
+tData = zeros(1,ceil(numIter/dataFreq)); % time data
+specSum = zeros(1,ceil(numIter/dataFreq)); % total species count data
+dataIndex = 2; % index for storing data, index 1 is the initial conditions
 
 
 % the matrix sij stores which metabolites each species consumes
@@ -47,18 +48,28 @@ for i=1:numIter
     %disp("timestep: "+i);
     % first  a random time interval dt is generated
     %dt = exprnd(1/sum(species));
+    
     sumSpecies = sum(species);
+    if(sumSpecies == 0)
+        break
+    end
+    
     mu = 1/sumSpecies;
     dt = -mu .* log(rand);
     
-    % saving dt data
-    dtData(i) = dt;
+%     if(sumSpecies ~= 0)
+%         mu = 1/sumSpecies;
+%         dt = -mu .* log(rand);
+%     else
+%         dt = 1;
+%     end
+    
     
     % metabolites are added
     r(u) = r(u) +my*dt;
     
-    % a species is randomly selected to reproduce or die
-    % the probability that species Sij is selected is Sij(t)/N(t)
+    % a species is randomly selected to reproduce or die.
+    % The probability that species Sij is selected is Sij(t)/N(t)
     % where N(t) is the total population.
     randN = rand*sumSpecies;
     %disp("randN: " + randN);
@@ -78,9 +89,9 @@ for i=1:numIter
     %disp("dt = " + dt);
     %disp("selectedS: " + selectedS)
     %disp(" species: ")
-    % disp(species)
-    % disp("resources at i: ")
-    % disp(r);
+    %disp(species)
+    %disp("resources at i: ")
+    %disp(r);
     %disp("added resources dt*my: " + dt*my);
     
     % Species selectedS reproduces with probability q(Rj(t),Ri(t))
@@ -177,27 +188,34 @@ for i=1:numIter
     %disp("--------------------------")
     
     % time is updated and data is stored
-    tData(i+1) = t +dt;
+    if(mod(i,dataFreq) == 0)
+        tData(dataIndex) = t +dt;
+        speciesData(:,dataIndex) = species;
+        rData(:,dataIndex)=r;
+        specSum(dataIndex) = sum(species);
+        dataIndex = dataIndex +1;
+    end
     t = t+dt;
-    speciesData(:,i) = species;
-    rData(:,i)=r;
-    
     %w = input('press Enter');
 end
-disp("lapsed runtime: " + toc);
+%disp("lapsed runtime: " + toc);
 
 
 %% plots
-figure(2)
-specTransp = speciesData';
-area(tData(1:200:end),specTransp(1:200:end,:))
+figure(1)
+%specTransp = speciesData';
+area(tData(1,1:1:end),speciesData(:,1:1:end)')
 xlabel('time');
 ylabel('nr individuals');
 legend('species 11','species 21','species 22')
 
+figure(2)
+plot(tData,specSum)
+xlabel('time');
+ylabel('# individuals');
+
 figure(3)
-rTranspose = rData';
-plot(tData(1:50000),rTranspose(1:50000,:))
+plot(tData,rData)
 xlabel('time');
 ylabel('resources');
 legend('metabolite 1','mteabolite 2')
